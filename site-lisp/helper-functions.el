68e2e96c17c0673389f884e15b8f448f3a2552ea
Inderjit Gill <email@indy.io>
2012-01-25 20:45:12 +0000

    moved clojurescript elisp code into helper-functions

--- site-lisp/helper-functions.el
+++ site-lisp/helper-functions.el
@@ -42,4 +42,60 @@
                (cons 'font
                      (isg-val 'default-font))))
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Allow input to be sent to somewhere other than inferior-lisp
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defvar clojure-repl-shell-name "*repl-shell*")
+
+(defun isg-start-clojure-repl-shell ()
+  "start a new clojure repl shell"
+  (interactive)
+  (isg-start-shell clojure-repl-shell-name))
+
+
+;; This is a total hack: we're hardcoding the name of the shell buffer
+(defun shell-send-input (input)
+  "Send INPUT into the *shell* buffer and leave it visible."
+  (save-selected-window
+    (switch-to-buffer-other-window clojure-repl-shell-name)
+    (goto-char (point-max))
+    (insert input)
+    (comint-send-input)))
+
+(defun defun-at-point ()
+  "Return the text of the defun at point."
+  (apply #'buffer-substring-no-properties
+         (region-for-defun-at-point)))
+
+(defun region-for-defun-at-point ()
+  "Return the start and end position of defun at point."
+  (save-excursion
+    (save-match-data
+      (end-of-defun)
+      (let ((end (point)))
+        (beginning-of-defun)
+        (list (point) end)))))
+
+(defun expression-preceding-point ()
+  "Return the expression preceding point as a string."
+  (buffer-substring-no-properties
+   (save-excursion (backward-sexp) (point))
+   (point)))
+
+(defun shell-eval-last-expression ()
+  "Send the expression preceding point to the *shell* buffer."
+  (interactive)
+  (shell-send-input (expression-preceding-point)))
+
+(defun shell-eval-defun ()
+  "Send the current toplevel expression to the *shell* buffer."
+  (interactive)
+  (shell-send-input (defun-at-point)))
+
+
+
 (provide 'helper-functions)
