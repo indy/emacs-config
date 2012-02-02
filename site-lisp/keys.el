7d9474c0b494bb539f8bbadf14b3c6e8ad4ded06
Inderjit Gill <email@indy.io>
2012-02-02 12:09:05 +0000

    common typo of C-cC-v now also invokes magit-status

--- site-lisp/keys.el
+++ site-lisp/keys.el
@@ -27,6 +27,7 @@
 (global-set-key "\M-j" 'eval-print-last-sexp)
 
 (global-set-key "\C-cv" 'magit-status)
+(global-set-key "\C-c\C-v" 'magit-status)
 
 (global-set-key [f1] 'isg-start-shell)
 (global-set-key [f2] 'isg-start-eshell)
