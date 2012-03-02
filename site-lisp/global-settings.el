15d97f8193b5bf4249d93c205d881abb93df3a15
Inderjit Gill <email@indy.io>
2012-03-02 15:20:47 +0000

    auto-complete configuration in global-settings

--- site-lisp/global-settings.el
+++ site-lisp/global-settings.el
@@ -1,3 +1,19 @@
+
+
+; auto-complete configuration
+
+; load the default configuration
+(require 'auto-complete-config)
+; Make sure we can find the dictionaries
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/external/auto-complete/dict")
+; Use dictionaries by default
+(setq-default ac-sources (add-to-list 'ac-sources 'ac-source-dictionary))
+(global-auto-complete-mode t)
+; Start auto-completion after 2 characters of a word
+(setq ac-auto-start 2)
+; case sensitivity is important when finding matches
+(setq ac-ignore-case nil)
+
 (show-paren-mode t)
 (global-font-lock-mode t)
 
