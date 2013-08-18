1fb6966c715de3c011ff5ad7307b6a3093c169ef
Inderjit Gill <email@indy.io>
2013-08-18 16:24:00 +0100

    flymake mode on for js

--- site-lisp/mode-configs.el
+++ site-lisp/mode-configs.el
@@ -23,7 +23,7 @@
 (require 'flymake-node-jshint)
 (setq js-indent-level 2)
 (setq flymake-node-jshint-config "~/.emacs.d/site-lisp/jshint-config.json")
-;(add-hook 'js-mode-hook (lambda () (flymake-mode 1)))
+(add-hook 'js-mode-hook (lambda () (flymake-mode 1)))
 
 ;;; go mode
 (require 'go-mode-load)
