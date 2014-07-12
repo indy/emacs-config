543d9d8b9273b3b7af14fd872b8fcc7128048802
Inderjit Gill <email@indy.io>
2014-07-12 10:40:20 +0100

    removed modes to languages I don't use

--- site-lisp/mode-configs.el
+++ site-lisp/mode-configs.el
@@ -1,16 +1,18 @@
-(require 'coffee-mode)
-(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+
+;;;(require 'coffee-mode)
+;;;(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
 
 ;;;(require 'typescript)
 ;;;(add-to-list 'auto-mode-alist '("\\.ts$" . typescript-mode))
 
+(add-to-list 'auto-mode-alist '("\\.seni$" . scheme-mode))
 
 (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.less$" . css-mode))
 (add-to-list 'auto-mode-alist '("\\.scss$" . sass-mode))
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.gradle$" . groovy-mode))
+;(add-to-list 'auto-mode-alist '("\\.gradle$" . groovy-mode))
 
 (autoload 'imbue-mode "imbue" nil t)
 (add-to-list 'auto-mode-alist '("\\.imd$" . imbue-mode))
@@ -23,11 +25,11 @@
 (require 'flymake-node-jshint)
 (setq js-indent-level 2)
 (setq flymake-node-jshint-config "~/.emacs.d/site-lisp/jshint-config.json")
-(add-hook 'js-mode-hook (lambda () (flymake-mode 1)))
+;(add-hook 'js-mode-hook (lambda () (flymake-mode 1)))
 
 ;;; go mode
-(require 'go-mode-load)
-(add-hook 'before-save-hook #'gofmt-before-save)
+;(require 'go-mode)
+;(add-hook 'before-save-hook #'gofmt-before-save)
 
 ;;; org-mode
 (add-hook 'org-mode-hook 'soft-wrap-lines)
@@ -69,6 +71,13 @@ in current buffer."
 ;;; clojure
 (add-hook 'clojure-mode-hook (lambda () 
                                (pretty-fn)))
+; | Keyboard Shortcut | Description                                    |
+; |-------------------+------------------------------------------------|
+; | C-c C-x n         | Checks the entire ns, printing errors          |
+; | C-c C-x f         | Checks the preceding form or symbol, as in cf  |
+; | C-c C-a v         | Inserts (ann ..) form above the top level expr |
+; | C-c C-a f         | Wraps the current form with (ann-form ... t)   |
+(add-hook 'clojure-mode-hook 'typed-clojure-mode)
 
 ;;; clojurescript
 (add-hook 'clojurescript-mode-hook 'er/add-clojure-mode-expansions)
