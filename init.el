7d2e3fcfa2a702c1694f655f9294141cc0c01d98
Inderjit Gill <email@indy.io>
2017-02-18 14:53:34 +0000

    better counsel keyboard commands

--- init.el
+++ init.el
@@ -118,16 +118,17 @@
 (use-package counsel
   :init
   (global-set-key (kbd "M-x") 'counsel-M-x)
+  (global-set-key (kbd "C-M-s") 'counsel-git-grep)
   (global-set-key (kbd "C-x C-f") 'counsel-find-file)
   (global-set-key (kbd "C-x C-g") 'counsel-git)
-  (global-set-key (kbd "C-M-s") 'counsel-ag)
+  (global-set-key (kbd "C-x C-h") 'counsel-ag)
+  
   ;; I don't use these bindings - should learn what they do one day
   (global-set-key (kbd "<f1> f") 'counsel-describe-function)
   (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
   (global-set-key (kbd "<f1> l") 'counsel-find-library)
   (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
   (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-  (global-set-key (kbd "C-c j") 'counsel-git-grep)
   (global-set-key (kbd "C-x l") 'counsel-locate)
   (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
   (define-key read-expression-map (kbd "C-r") 'counsel-expression-history))
